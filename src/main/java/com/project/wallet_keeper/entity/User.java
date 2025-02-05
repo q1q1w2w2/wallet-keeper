1412a8e3e0b69d5afe30c8e00d44213ee6cc7853
q1q1w2w2 <xmrrhdwjdqls@gmail.com>
2025-02-05 18:02:19 +0900

    Fix: 캐시 직렬화 문제 해결

--- src/main/java/com/project/wallet_keeper/entity/User.java
+++ src/main/java/com/project/wallet_keeper/entity/User.java
@@ -6,6 +6,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
@@ -13,7 +14,7 @@ import java.time.LocalDateTime;
 @Table(name = "\"user\"")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class User {
+public class User implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
